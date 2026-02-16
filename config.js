a16058909b13dca3c181b345b0182adccc634fe9
974network <974network@gmail.com>
2026-02-16 14:46:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzVWVpwFIKt1d2uv_bdaJ9OpDozgO2NxO00Ftoj5z5oXr3ISROAcrubu6sOnTf7iMvI/exec";
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx5E4dJbQ_tEf70Hw6qTMkoHzUvKtW2AgIept8VfQU0tZjqgIk-5f797QwsAu9MENa2/exec";
