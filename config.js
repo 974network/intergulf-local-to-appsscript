037dd2f70a574e32fbcdfba805647b19a9f49366
974network <974network@gmail.com>
2026-02-16 14:58:16 +0300

    Update APP_SCRIPT_URL to new endpoint

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzyHOBRQ2xVVBH_aiDL_IiJDzqKNAAhsQCe7a03M3-viKm_HGuSLV5f8HmoUg8DeJv_/exec";
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwwP2JnY9C8IKa0AGurFXJki_zoRrqbiMY91EZirj-MLnPpWh5J9KtmhZcelrK6K4oL/exec";
