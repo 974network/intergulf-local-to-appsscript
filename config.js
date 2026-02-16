7a894a37b5cf57fbc69a10310e7b3fc455b3f7e2
974network <974network@gmail.com>
2026-02-16 15:21:17 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Paste your Apps Script Web App URL here (ends with /exec)
-window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxH0jfPCR-DtxXHjILYSop2VX_w1zfLF5jS4pO6yuVJhKFhsY89en2PYhlKPyl99gQc/exec";
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxfaU0xUEMLWvaBKScYKfx4TPQ8hUFTqz4TpWZiaP_-Nk2lYqnBQP_gPXxHJWICXe1a/exec";
