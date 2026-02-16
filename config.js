4925d2b02bee5270855137c883954edc88e9a59e
974network <974network@gmail.com>
2026-02-16 12:47:32 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+// Paste your Apps Script Web App URL here (ends with /exec)
+window.APP_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyYX_ROBV24fW6KfWOjUXYN7oWedrVeI9Ktq6YA2NCOd7ESCIKbC6FE4p-fMtRjU1g1/exec";
